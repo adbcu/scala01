4fcfc1782e7a900b45b0afc00865f806cbb814a2
Evgeniy <evgeniy.potseluev@gmail.com>
2022-02-08 23:20:16 +0300

    -

---
+++ src/myScala01.scala
@@ -0,0 +1,11 @@
+class Student(id: Int, val name: String) {
+  val uni = "University"
+  print("Hello")
+}
+
+
+object myScala01 extends App {
+
+  val student = new Student(name = "Bob", id = 0)
+
+}
