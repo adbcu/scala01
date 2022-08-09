81d372ab57566c8568f9bd99d79821face621e99
evgeniy.potseluev@gmail.com <TfQdK9n7mW9UgDz>
2022-08-09 09:12:29 +0300

    chapter02

---
+++ src/scala_for_impatient/chapter03_arrays/excercise_01.scala
@@ -0,0 +1,12 @@
+package scala_for_impatient.chapter03_arrays
+
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
+
+object excercise_01 extends App{
+//  var arr = new Array[Int](5)
+//  var arr2 = Array(5)
+
+
+
+}
