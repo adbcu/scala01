8dcf7694a12090d495fa8cc93b0ddb34318fbd22
evgeniy.potseluev@gmail.com <TfQdK9n7mW9UgDz>
2022-08-15 09:01:50 +0300

    chapter05

---
+++ src/scala_for_impatient/chapter05_classes/excercise_05.scala
@@ -0,0 +1,24 @@
+package scala_for_impatient.chapter05_classes
+
+import scala.beans.BeanProperty
+
+object excercise_05 extends App {
+  class Student(@BeanProperty var gender: String = "n/a") {
+
+    @BeanProperty val name: String = ""
+    @BeanProperty var id: Long = 0
+
+  }
+
+  val student = new Student("male")
+  println(s"student.id = ${student.id}")
+  student.id = 1
+  println(s"student.id = ${student.id}")
+  student.setId(2)
+  println(s"student.id = ${student.id}")
+
+
+
+
+
+}
